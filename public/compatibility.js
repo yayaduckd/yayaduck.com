40a41150a728980c23f8be2dd2803a5f1f20bc4d
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2023-08-01 21:48:28 +0200

    make controller only show for mobile

---
+++ public/compatibility.js
@@ -0,0 +1,9 @@
+// Check if on mobile
+//
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    document.querySelector('.controller').style.display = 'block';
+}
