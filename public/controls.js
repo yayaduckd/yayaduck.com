8ad34bebf47e1b465f2142af00525e2a34354c49
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2023-08-01 21:48:03 +0200

    optimize by redrawing individual tiles instead of whole map

--- public/controls.js
+++ public/controls.js
@@ -1,4 +1,4 @@
-import { entities, mapData, drawMap } from './map.js';
+import { entities, mapData, drawMap, drawCell } from './map.js';
 
 // Constants
 const MOVE_UP = ['ARROWUP', 'W'];
@@ -8,42 +8,41 @@ const MOVE_RIGHT = ['ARROWRIGHT', 'D'];
 
 // Event listener for keydown events
 document.addEventListener('keydown', function (event) {
-    event.preventDefault();
     const key = event.key.toUpperCase();
 
     // Check which movement key was pressed
     if (MOVE_UP.includes(key)) {
         up();
+        event.preventDefault();
     } else if (MOVE_DOWN.includes(key)) {
         down();
+        event.preventDefault();
     } else if (MOVE_LEFT.includes(key)) {
         left();
+        event.preventDefault();
     } else if (MOVE_RIGHT.includes(key)) {
         right();
+        event.preventDefault();
     }
 
     // Redraw the map with the updated entity position
-    drawMap(mapData);
+    // drawMap(mapData);
 });
 
 function up() {
     moveEntity(entities.duck, 0, -1);
-    drawMap(mapData);
 }
 
 function down() {
     moveEntity(entities.duck, 0, 1);
-    drawMap(mapData);
 }
 
 function left() {
     moveEntity(entities.duck, -1, 0);
-    drawMap(mapData);
 }
 
 function right() {
     moveEntity(entities.duck, 1, 0);
-    drawMap(mapData);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -58,12 +57,19 @@ document.addEventListener('DOMContentLoaded', () => {
     crossRight.addEventListener('mousedown', right);
   });
 
+
 /**
  * Move an entity by the given deltas, while respecting map boundaries
  */
 function moveEntity(entity, deltaX, deltaY) {
+    const oldX = entity.x;
+    const oldY = entity.y;
     const newX = entity.x + deltaX;
     const newY = entity.y + deltaY;
+
+    if (newX < 0 || newX >= mapData.width || newY < 0 || newY >= mapData.height) {
+        return;
+    }
 
     const newTile = mapData.map[newY][newX];
 
@@ -73,4 +79,6 @@ function moveEntity(entity, deltaX, deltaY) {
 
     entity.x = newX;
     entity.y = newY;
+    drawCell(oldY, oldX, mapData.map);
+    drawCell(newY, newX, mapData.map);
 }
--- public/map.js
+++ public/map.js
@@ -1,8 +1,8 @@
 // Constants
 const MAP_VISUAL_FILE_DEFAULT = "./map/visual.txt";
 
-// Exporting the map data object
-// export const mapData = initMapData();
+const CHANGING_TILE_CLASSES = ["duck"];
+
 let mapData;
 
 export var entities = {
@@ -10,9 +10,9 @@ export var entities = {
         x: 26,
         y: 4,
         icon: " ",
-        class: "duck"
+        class: "duck",
+        tile: ".",
     },
-    // Add more entities if needed
 };
 
 async function fetchFileContent(filename) {
@@ -33,10 +33,29 @@ async function initMapData() {
     }
 }
 
+function initEmptyHTMLMap(width, height) {
+    const mapElement = document.getElementById("map");
+    mapElement.innerHTML = "";
+
+    for (let row = 0; row < height; row++) {
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        rowDiv.classList.add("row-" + row); // Include this line
+        for (let col = 0; col < width; col++) {
+            const colSpan = document.createElement("span");
+            colSpan.classList.add("col");
+            colSpan.classList.add("col-" + col);
+            colSpan.classList.add("row-" + row);
+            colSpan.innerHTML = "&nbsp;";
+            rowDiv.appendChild(colSpan);
+        }
+        mapElement.appendChild(rowDiv);
+    }
+}
+
 export function drawMap(mapData) {
     const { width, height, map } = mapData;
     const mapElement = document.getElementById("map");
-    mapElement.innerHTML = "";
 
     for (let row = 0; row < height; row++) {
         drawRow(row, map, mapElement, width);
@@ -52,31 +71,34 @@ function drawRow(row, map, mapElement, width) {
     mapElement.appendChild(rowDiv);
 }
 
-function drawCell(row, col, map, rowDiv) {
-    const colSpan = document.createElement("span");
-    colSpan.classList.add("col");
-    colSpan.classList.add("col-" + col);
-    colSpan.classList.add("row-" + row);
+export function drawCell(row, col, map) {
+    const element = document.querySelector(`.row-${row} .col-${col}`);
+    CHANGING_TILE_CLASSES.forEach((className) => {
+        element.classList.remove(className);
+    });
 
     let nextChar = map[row][col];
-    nextChar = handleEntities(col, row, nextChar, colSpan);
+    nextChar = handleEntities(col, row, nextChar, element, map);
 
     if (nextChar === " " || nextChar === undefined) {
-        colSpan.classList.add("empty");
+        element.classList.add("empty");
         nextChar = "&nbsp;";
+    } else {
+        element.classList.remove("empty");
     }
 
-    colSpan.innerHTML = nextChar;
-    rowDiv.appendChild(colSpan);
+    element.innerHTML = nextChar;
 }
 
-function handleEntities(col, row, char, element) {
+
+function handleEntities(col, row, char, element, map) { 
     for (const key in entities) {
         const entity = entities[key];
         if (entity.x === col && entity.y === row) {
             element.classList.add(entity.class);
             return entity.icon;
         }
+        entity.tile = map[entity.y][entity.x];
     }
     return char;
 }
@@ -84,6 +106,7 @@ function handleEntities(col, row, char, element) {
 initMapData()
   .then((data) => {
     mapData = data;
+    initEmptyHTMLMap(data.width, data.height);
     drawMap(data);
   })
   .catch(console.error);
